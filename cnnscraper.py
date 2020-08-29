a570ca94c7f7d70d52f508e6a93435080237eb8b
Thanh Nhan Le <thanhnhanle1412@gmail.com>
2020-08-29 18:59:17 +0800

    Adding link

--- cnnscraper.py
+++ cnnscraper.py
@@ -10,7 +10,7 @@ class cnnsearch:
     def __init__(self, keyword, pages):
         current_page = 1
         from_post = (current_page - 1) * 10
-        bot = webdriver.Chrome(chrome_options=chrome_options)
+        bot = webdriver.Chrome(options=chrome_options)
         new_keyword = keyword.replace(' ', '+')
         for x in range(pages):
             bot.get ('https://edition.cnn.com/search?q='+ new_keyword+ '&page=' + str(current_page)+ '&from='+ str(from_post))
@@ -24,8 +24,10 @@ class cnnsearch:
             for article in article_list:
                 title = article.find_element_by_class_name('cnn-search__result-headline')
                 body = article.find_element_by_class_name('cnn-search__result-body')
+                link = article.find_element_by_link_text(title.text)
                 print('\nTitle\n')
                 print(title.text)
+                print(link.get_attribute('href'))
                 print('\nBody \n')
                 print(body.text[:1000])
                 print('\n==========================================================')
